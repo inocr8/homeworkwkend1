bc2241bd1b33eea5194e045d98060467614f7026
Euan Walls <apps@inocr8.co>
2015-11-23 08:59:10 +0000

    last one in for weekend homework

--- warehouse_picker_functions.rb
+++ warehouse_picker_functions.rb
@@ -1,51 +1,11 @@
+require 'pry-byebug'
+
 WAREHOUSE = [
   {
     rack_row: "a",
-    bay_number: 1, 
-    rack_bay_position: "a1",
-    product_name: "needle"
-  },
-  {
-    rack_row: "a",
-    bay_number: 2, 
-    rack_bay_position: "a2",
-    product_name: "stop sign"
-  },
-  {
-    rack_row: "a",
-    bay_number: 3, 
-    rack_bay_position: "a3",
-    product_name: "blouse"
-  },
-  {
-    rack_row: "a",
-    bay_number: 4, 
-    rack_bay_position: "a4",
-    product_name: "hanger"
-  },
-  {
-    rack_row: "a",
-    bay_number: 5, 
-    rack_bay_position: "a5",
-    product_name: "rubber duck"
-  },
-  {
-    rack_row: "a",
-    bay_number: 6, 
-    rack_bay_position: "a6",
-    product_name: "shovel"
-  },
-  {
-    rack_row: "a",
-    bay_number: 7, 
-    rack_bay_position: "a7",
-    product_name: "bookmark"
-  },
-  {
-    rack_row: "a",
-    bay_number: 8, 
-    rack_bay_position: "a8",
-    product_name: "hanger"
+    bay_number: 10, 
+    rack_bay_position: "a10",
+    product_name: "rubber band"
   },
   {
     rack_row: "a",
@@ -55,69 +15,51 @@ WAREHOUSE = [
   },
   {
     rack_row: "a",
-    bay_number: 10, 
-    rack_bay_position: "a10",
-    product_name: "rubber band"
-  },
-  {
-    rack_row: "b",
-    bay_number: 1, 
-    rack_bay_position: "b1",
-    product_name: "tyre swing"
-  },
-  {
-    rack_row: "b",
-    bay_number: 2, 
-    rack_bay_position: "b2",
-    product_name: "sharpie"
-  },
-  {
-    rack_row: "b",
-    bay_number: 3, 
-    rack_bay_position: "b3",
-    product_name: "picture frame"
-  },
-  {
-    rack_row: "b",
-    bay_number: 4, 
-    rack_bay_position: "b4",
-    product_name: "photo album"
-  },
-  {
-    rack_row: "b",
-    bay_number: 5, 
-    rack_bay_position: "b5",
-    product_name: "nail filer"
-  },
-  {
-    rack_row: "b",
-    bay_number: 6, 
-    rack_bay_position: "b6",
-    product_name: "tooth paste"
-  },
-  {
-    rack_row: "b",
-    bay_number: 7, 
-    rack_bay_position: "b7",
-    product_name: "bath fizzers"
-  },
-  {
-    rack_row: "b",
     bay_number: 8, 
-    rack_bay_position: "b8",
-    product_name: "tissue box"
+    rack_bay_position: "a8",
+    product_name: "hanger"
   },
   {
-    rack_row: "b",
-    bay_number: 9, 
-    rack_bay_position: "b9",
-    product_name: "deodorant"
+    rack_row: "a",
+    bay_number: 7, 
+    rack_bay_position: "a7",
+    product_name: "bookmark"
   },
   {
-    rack_row: "b",
-    bay_number: 10, 
-    rack_bay_position: "b10",
-    product_name: "cookie jar"
+    rack_row: "a",
+    bay_number: 6, 
+    rack_bay_position: "a6",
+    product_name: "shovel"
+  },
+  {
+    rack_row: "a",
+    bay_number: 5, 
+    rack_bay_position: "a5",
+    product_name: "rubber duck"
+  },
+  {
+    rack_row: "a",
+    bay_number: 4, 
+    rack_bay_position: "a4",
+    product_name: "hanger"
+  },
+  {
+    rack_row: "a",
+    bay_number: 3, 
+    rack_bay_position: "a3",
+    product_name: "blouse"
+  },
+  {
+    rack_row: "a",
+    bay_number: 2, 
+    rack_bay_position: "a2",
+    product_name: "stop sign"
+  },
+  {
+    rack_row: "a",
+    bay_number: 1, 
+    rack_bay_position: "a1",
+    product_name: "needle"
   },
   {
     rack_row: "c",
@@ -179,20 +121,129 @@ WAREHOUSE = [
     rack_bay_position: "c10",
     product_name: "leg warmers"
   },
+  {
+    rack_row: "b",
+    bay_number: 1, 
+    rack_bay_position: "b1",
+    product_name: "tyre swing"
+  },
+  {
+    rack_row: "b",
+    bay_number: 2, 
+    rack_bay_position: "b2",
+    product_name: "sharpie"
+  },
+  {
+    rack_row: "b",
+    bay_number: 3, 
+    rack_bay_position: "b3",
+    product_name: "picture frame"
+  },
+  {
+    rack_row: "b",
+    bay_number: 4, 
+    rack_bay_position: "b4",
+    product_name: "photo album"
+  },
+  {
+    rack_row: "b",
+    bay_number: 5, 
+    rack_bay_position: "b5",
+    product_name: "nail filer"
+  },
+  {
+    rack_row: "b",
+    bay_number: 6, 
+    rack_bay_position: "b6",
+    product_name: "tooth paste"
+  },
+  {
+    rack_row: "b",
+    bay_number: 7, 
+    rack_bay_position: "b7",
+    product_name: "bath fizzers"
+  },
+  {
+    rack_row: "b",
+    bay_number: 8, 
+    rack_bay_position: "b8",
+    product_name: "tissue box"
+  },
+  {
+    rack_row: "b",
+    bay_number: 9, 
+    rack_bay_position: "b9",
+    product_name: "deodorant"
+  },
+  {
+    rack_row: "b",
+    bay_number: 10, 
+    rack_bay_position: "b10",
+    product_name: "cookie jar"
+  },
 ]
 
+####  SECTION A - Single bay/item
+### Given a bay, returns the item in that bay.
 def item_at_bay(row, bay)
-  location = WAREHOUSE.select{ |x| x[:rack_row] == row && x[:bay_number] == bay }
-  return location[0][:product_name]
+  product = WAREHOUSE.select{ |item| item[:rack_row] == row && item[:bay_number] == bay }
+  return product[0][:product_name]
 end
 
-
-def location_of_bay(item)
-  location = WAREHOUSE.select{ |x| x[:product_name] == item }
+### Given an item return the bay that it is in.
+def location_of_bay(bay)
+  location = WAREHOUSE.select{ |item| item[:product_name] == bay }
   return location[0][:rack_bay_position]
 end
 
+####  SECTION B - Multiple bays/items
+### Given a list of bays, list the items in those bays.
+def item_at_bays(loc1, loc2, loc3)
+  product_bays = WAREHOUSE.select{ |bays| bays[:rack_bay_position] == loc1 || bays[:rack_bay_position] == loc2 ||  bays[:rack_bay_position] == loc3 }
+  product_bays.map{ |bays| bays[:product_name] }.join(", ")
+end 
 
+### Given a list of items find the bays.
+def location_of_bays(item1, item2, item3)
+  location_bays = WAREHOUSE.select{ |bays| bays[:product_name] == item1 || bays[:product_name] == item2 ||  bays[:product_name] == item3 }
+  location_bays.map{ |bays| bays[:rack_bay_position] }.join(", ")
+end
+
+
+#### SECTION C - Advanced
+
+### Given a list of bays, list the items in those bays, and calculate the distance from the two furthest apart bays. For instance:
+
+## given "b5, b10, and b6", determine that the products are "nail filer, cookie jar, and tooth paste", and they're five bays apart
+def distance_of_items(loc1, loc2, loc3)
+  items = [loc1, loc2, loc3]
+  low, high = items.map { |item| find_location_bays(item) }.minmax
+  high - low
+end
+
+def find_location_bays(bay)
+  WAREHOUSE.index { |position| position[:rack_bay_position] == bay }
+end
+
+## given "b3, c7, c9 and a3", determine that the products are "picture frame, paint brush, shoe lace, and blouse", and they're 15 bays apart.)
+def distance_of_bays(loc1, loc2, loc3, loc4)
+  items = [loc1, loc2, loc3, loc4]
+  low, high = items.map { |item| find_location_bays(item) }.minmax
+  high - low
+end
+
+## given "b3, c7, c9 and a3", determine that the products are "picture frame, paint brush, shoe lace, and blouse", and they're 15 bays apart.)
+def item_at_bays_more(loc1, loc2, loc3, loc4)
+  product_bays_more = WAREHOUSE.select{ |bays| bays[:rack_bay_position] == loc1 || bays[:rack_bay_position] == loc2 ||  bays[:rack_bay_position] == loc3 || bays[:rack_bay_position] == loc4 }
+  product_bays_more.map{ |bays| bays[:product_name] }.join(", ")
+end 
+
+### Given a list of products, find the bays that need to be visited, and order them in the order they need to be visited from entrance to exit. For instance:
+
+## given "shoe lace, rusty nail, leg warmers", determine that those items need to be collected from "c1, c9, and c10"
+def order_of_visit(item1, item2, item3)
+  
+end
 
 
 
--- warehouse_picker_runner.rb
+++ warehouse_picker_runner.rb
@@ -11,11 +11,13 @@ puts "The nail filer can be found at #{found_bay}, expect it to be b5.\n\n"
 
 
 ####  SECTION B - Multiple bays/items
-### Given a list of bays, list the items in those bays
-
+### Given a list of bays, list the items in those bays.
+found_items = item_at_bays('b5', 'b10', 'b6')
+puts "The items at locations b5, b10 and b6 are #{found_items}, expecting them to be nail filer, cookie jar and toothpaste.\n\n"
 
 ### Given a list of items find the bays.
-
+found_bays = location_of_bays('shoe lace', 'rusty nail', 'leg warmers')
+puts "The items shoe lace, rusty nail and leg warmers are found at #{found_bays}, expecting them to be c1, c9 and c10.\n\n"
 
 
 #### SECTION C - Advanced
@@ -23,11 +25,20 @@ puts "The nail filer can be found at #{found_bay}, expect it to be b5.\n\n"
 ### Given a list of bays, list the items in those bays, and calculate the distance from the two furthest apart bays. For instance:
 
 ## given "b5, b10, and b6", determine that the products are "nail filer, cookie jar, and tooth paste", and they're five bays apart
+found_items_distance = distance_of_items( 'b5', 'b10', 'b6' )
+puts "The items at locations b5, b10 and b6 are #{found_items}, expecting them to be nail filer, cookie jar and toothpaste.  They are #{found_items_distance} bays apart.\n\n"
 
 ## given "b3, c7, c9 and a3", determine that the products are "picture frame, paint brush, shoe lace, and blouse", and they're 15 bays apart.)
+found_items = item_at_bays_more( 'b3', 'c7', 'c9', 'a3' )
+found_bays_distance = distance_of_bays( 'b3', 'c7', 'c9', 'a3' )
+puts "The items at locations a3, c7, c9 and b3 are #{found_items}, expecting them to be blouse, paint brush, shoe lace and picture frame. They are #{found_bays_distance} bays apart.\n\n"
 
 ### Given a list of products, find the bays that need to be visited, and order them in the order they need to be visited from entrance to exit. For instance:
 
 ## given "shoe lace, rusty nail, leg warmers", determine that those items need to be collected from "c1, c9, and c10"
+found_bays = location_of_bays('shoe lace', 'rusty nail', 'leg warmers')
+order = order_of_visit('rusty nail', 'shoe lace', 'leg warmers')
+puts "The bays containing the shoe lace, rusty nail and leg warmers are #{found_bays}, expecting them to be c9, c1, and c10. These items should be collected in this order: #{order}"
+puts "\n"
 
 ## given "hanger, deodorant, candy wrapper, rubber band", determine that those items need to be collected from "a10, a4, c8, and b9"
